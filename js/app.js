da82fd1132031c8de77fc51b225569afd0469692
mochicat8 <mochicat8@gmail.com>
2017-08-24 21:26:13 -0700

    fixes overlapping divs

--- js/app.js
+++ js/app.js
@@ -1,33 +1,35 @@
-var locations = [{
-    title: 'i-Tea',
+var locations =
+        [
+            {
+    title: "i-Tea",
     location: {
-      "lat": 37.7635227,
-      "lng": -122.4811778
+        "lat": 37.7635227,
+        "lng": -122.4811778
     }
   },
   {
-    title: 'Purple Kow',
+    title: "Purple Kow",
     location: {
       "lat": 37.7758612,
       "lng": -122.4977786
     }
   },
   {
-    title: 'Wonderful Dessert and Cafe',
+    title: "Wonderful Dessert and Cafe",
     location: {
       "lat": 37.7633039,
       "lng": -122.4799618
     }
   },
   {
-    title: 'Boba Guys',
+    title: "Boba Guys",
     location: {
       "lat": 37.759997,
       "lng": -122.4211236
     }
   },
   {
-    title: 'Teaspoon',
+    title: "Teaspoon",
     location: {
       "lat": 37.7963054,
       "lng": -122.4221592
@@ -42,47 +44,46 @@ function myFunction() {
   } else {
     x.className = "filters";
   }
+}
 
+var Pin = function(i, map, title, position, view) {
+  "use strict";
+  var thisMarker = this;
+  this.title = title;
+  this.position = position;
+  this.isVisible = ko.observable(true);
+  this.marker = new google.maps.Marker({
+    map: map,
+    position: position,
+    title: title,
+    animation: google.maps.Animation.DROP,
+    id: i,
+    visible: true
+  });
 
-
+  this.isVisible.subscribe(function(currentState) {
+    if (currentState) {
+      thisMarker.marker.setMap(map);
+    } else {
+      thisMarker.marker.setMap(null);
+    }
+  });
 };
+
 
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: {
       lat: 37.764245,
       lng: -122.423661
     },
-    zoom: 10
+    mapTypeControl: false,
+    fullscreenControl: false,
+    zoom: 2
   });
+
   var self = this;
-
-  var Pin = function(i, map, title, position, view) {
-    var thisMarker = this;
-    this.title = title;
-    this.position = position;
-    this.isVisible = ko.observable(true);
-    this.marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i,
-      visible: true
-    });
-
-    this.isVisible.subscribe(function(currentState) {
-      if (currentState) {
-        thisMarker.marker.setMap(map);
-      } else {
-        thisMarker.marker.setMap(null);
-      }
-    });
-  };
-
-
-
 
   function AppViewModel() {
     // Data
@@ -93,7 +94,7 @@ function initMap() {
     var bounds = new google.maps.LatLngBounds();
     this.largeInfowindow = new google.maps.InfoWindow();
     // These are the real estate listings that will be shown to the user.
-    // Normally we'd have these in a database instead.
+    // Normally we"d have these in a database instead.
     this.query = ko.observable("");
     this.filteredSearch = ko.computed(function() {
       var filter = self.query();
@@ -123,7 +124,7 @@ function initMap() {
       this.markers().push(pin);
 
       (function(pinIn) {
-        google.maps.event.addListener(pin.marker, 'click', function() {
+        google.maps.event.addListener(pin.marker, "click", function() {
           self.populateInfoWindow(pinIn, pin.marker);
         });
       })(pin);
@@ -163,27 +164,25 @@ function initMap() {
 
         var information;
         $.ajax(settings1, function() {
-          alert('done2');
+          alert("done2");
         }).done(function(response) {
           information = {
             name: response.response.venues[0].name,
             address: response.response.venues[0].location.formattedAddress,
             website: response.response.venues[0].url
           }
-          var html = '<div>' +
-            '<p>' + information.name + '</p>' +
-            '<p>' + information.address[0] + '</p>' +
-            '<p>' + information.address[1] + '</p>' +
-            '<p>' + information.website + '</p>' +
-            '</div>';
-
-
+          var html = "<div>" +
+            "<p>" + information.name + "</p>" +
+            "<p>" + information.address[0] + "</p>" +
+            "<p>" + information.address[1] + "</p>" +
+            "<p>" + information.website + "</p>" +
+            "</div>";
 
           self.largeInfowindow.setContent(html);
           self.largeInfowindow.open(map, pin.marker);
         });
         // Make sure the marker property is cleared if the infowindow is closed.
-        self.largeInfowindow.addListener('closeclick', function() {
+        self.largeInfowindow.addListener("closeclick", function() {
           self.largeInfowindow.marker.setAnimation(null);
           self.largeInfowindow.setMarker = null;
         });
